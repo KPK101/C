14e1f6c3a409a0c1e665df9f81cefb1830a5a4ee
Pranav <pranavkumarkota@gmail.com>
2024-10-11 18:19:57 -0400

    add same conv2D

--- convolution.c
+++ convolution.c
@@ -8,6 +8,9 @@ float** initMatrix(int xlen, int ylen){
 	x = malloc(xlen*sizeof(float*));
 	for(int i=0; i<xlen; i++){
 		x[i] = malloc(ylen*sizeof(float));
+		for(int j=0; j<ylen; j++){
+			x[i][j] = 0;
+		}
 	}
 	return x;
 }
@@ -16,11 +19,13 @@ float** initMatrix(int xlen, int ylen){
 
 void printMatrix(float** x, int xlen, int ylen){
 	//print the input matrix of shape (xlen, ylen)
+	
 	for(int i=0; i<xlen; i++){
+		printf("[ ");
 		for(int j=0; j<ylen; j++){
 			printf("%d ",(int)x[i][j]);
 		}
-		printf("\n");
+		printf("]\n");
 	}
 	printf("\n");
 }
@@ -28,19 +33,21 @@ void printMatrix(float** x, int xlen, int ylen){
 
 void print_vector(float *x, int size){
 	// Print an input vector of length "size"
+	printf("[ ");	
 	for(int i=0; i<size; i++){
-		printf("%f ",x[i]);
+		printf("%d ",(int)x[i]);
 	}
-	printf("\n");
+	printf("]\n");
 }
 
 
 void print_flip(float *x, int size){
 	// Print the flipped version of a vector - helps with visualizing flipped kernels
-		for(int i=0; i<size; i++){
-		printf("%f ",x[size-i-1]);
+	printf("[ ");	
+	for(int i=0; i<size; i++){
+		printf("%d ",(int)x[size-i-1]);
 	}
-	printf("\n");
+	printf("]\n");
 }
 
 
@@ -69,7 +76,7 @@ float ** conv2D(float** x, float **k,
 	int ylenx = xlenx - klenx + 1;
 	int yleny = xleny - kleny + 1;
 	// Create outmatrix y
-	printf("conv2D computation - output shape = (%d, %d)\n\n", ylenx, ylenx);
+	// printf("conv2D computation - output shape = (%d, %d)\n\n", ylenx, ylenx);
 	float **y = initMatrix(ylenx, yleny);
 	if(y==NULL){
 		printf("Unable to allocate memory :(");
@@ -104,14 +111,43 @@ return y;
 
 }
 
+float ** conv2D_same(float **x, float **k, int xlenx, int xleny, int klenx, int kleny){
+
+	// Convoltion kernel for same size
+	// Input padded
+	// Input is half to left and half to right
+	
+
+	int leftpadx = (int)(klenx-1)/2;
+	int rightpadx = klenx-1-leftpadx;
+	int leftpady = (int)(kleny-1)/2;
+	int rightpady = kleny-1-leftpady;
+
+	// printf("padded (%d,%d) on left and (%d,%d) on right\n",leftpadx,
+	// 	leftpadx, rightpadx, rightpady);
+	float ** xpad = initMatrix(xlenx + klenx -1, xleny + kleny -1);
+	// printf("Copying data from x to xpad\n");
+	for(int i=leftpadx; i<xlenx-(leftpadx); i++){
+		for(int j=leftpady; j<xleny-(leftpady); j++){
+			// printf("\tProcessing (%d,%d)\n",i,j);	
+			xpad[i][j] = x[i-leftpadx][j-leftpady];
+		}
+	}
+	// printf("Copied successfully!\n");
+
+	// printf("conv2D same computation - output shape = (%d, %d)\n\n", xlenx, xleny);
+	float ** y = conv2D(xpad, k, xlenx+klenx-1, xleny+kleny-1, klenx, kleny);
+	return y;
+}	
+
 void test_conv1d(void){
 
 	// Initialize an input vector - f, a kernel/filter - g, an output - y
 	// input size
 	// filter size
 	// each vector varies from 0/1 to vector_size(-0/-1)
-	int insize = 10;
-	int kernelsize = 5;
+	int insize = 5;
+	int kernelsize = 2;
 	float f[insize];
 	float g[kernelsize];
 
@@ -123,23 +159,24 @@ void test_conv1d(void){
 		f[i] = i;
 	}
 	for(int i=0; i<kernelsize; i++){
-		g[i] = 1;
+		g[i] = i+1;
 	}
 
 	// call the conv1d function - this performs valid convolution on the input array
 	conv1D(f, g, y, insize, kernelsize);
 
-	printf("Initiating 1D convolution...");
-	printf("Input size = %d, Kernel size = %d\n", insize, kernelsize);
+	printf("\nTesting 1D convolution...\n");
 	
-	printf("Input:\n");
+	// printf(" f size = %d\ng size = %d\n", insize, kernelsize);
+	
+	printf("f of size (%d):\n\n",insize);
 	print_vector(f,insize);
 	
-	printf("Flipped filter:\n");
+	printf("\n\"flipped\" g of size (%d):\n\n", kernelsize);
 	print_flip(g, kernelsize);
 
-	printf("The convolution operation is finished:\n");
-	printf("The output - of size %d is:\n", outsize);
+	// printf("The convolution operation is finished:\n");
+	printf("\ny of size (%d):\n\n", outsize);
 	print_vector(y, outsize);
 }
 
@@ -168,6 +205,8 @@ void test_conv2d(void){
 		}
 	}
 
+	printf("Testing 2D convolution...\n");
+
 	printf("x of shape (%d, %d) =\n\n", xlenx, xleny);
 	printMatrix(x, xlenx, xleny);
 
@@ -181,12 +220,18 @@ void test_conv2d(void){
 	printMatrix(y, xlenx - klenx + 1, xleny - kleny + 1);
 
 
+	float **y_same = conv2D_same(x, k, xlenx, xleny, klenx, kleny);
+	printf("y_same of shape (%d, %d) =\n\n", xlenx , xleny);
+	printMatrix(y_same, xlenx, xleny);
+
+
 }
 
 int main(void){
 
-
+test_conv1d();
+printf("\n");
 test_conv2d();
 
-// return 0;
+return 0;
 }
